52cb58beb0f740f4d817bafef4c911410c02331e
User <User@DESKTOP-6N6EHH6>
2020-11-21 22:30:09 +0700

    PBOIF2-10119067

---
+++ src/pboif2/pkg10119067/latihan51/gaji/karyawan/Karyawan.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119067.latihan51.gaji.karyawan;
+
+/**
+ *
+ * @author Danhago
+ * NAMA     : Andreas Danhago P.I
+ * KELAS    : IF2
+ * NIM      : 10119067
+ * Deskripsi: Gaji Karyawan
+ */
+public class Karyawan {
+    protected String nik,nama,jabatan;
+    protected int golongan;
+
+    public String getNik() {
+        return nik;
+    }
+
+    public void setNik(String nik) {
+        this.nik = nik;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getJabatan() {
+        return jabatan;
+    }
+
+    public void setJabatan(String jabatan) {
+        this.jabatan = jabatan;
+    }
+
+    public int getGolongan() {
+        return golongan;
+    }
+
+    public void setGolongan(int golongan) {
+        this.golongan = golongan;
+    }
+    
+}
